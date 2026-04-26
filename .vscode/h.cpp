b2822fea056d75a83576a23fbdb2c38c7b898d5c
yemu-se <yemanechekole@gmail.com>
2026-04-26 12:11:54 +0300

    already there!!

--- .vscode/h.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    cout<<"j";//g++ main.cpp src/*.cpp -I include -o scheduler.exe 
-//scheduler.exe 
-cout << "\nPress Enter to exit...";
-cin.get();
-}
